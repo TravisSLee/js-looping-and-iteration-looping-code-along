654facb134eca178bfd9377138ab0e11badabafb
TravisSLee <travisslee223@gmail.com>
2021-02-17 14:01:32 -0500

    commit tree 1247b7ff7456da924f2b03b67b51bcd8f6787b3f

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+
+
+let names = ["Ada", "Brendan", "Ali"];
+let holiday = "birthday";
+
+function writeCards(names, holiday) {
+    let messages = [];
+    for (let i = 0; i < names.length; i++) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful ${holiday} gift!`);
+    }
+    return messages;
+  };
+
+  function countDown(number) {
+    let i = 0; 
+    let subtract = number
+    while (i <= number) {
+        console.log(subtract);
+        subtract = subtract - 1;
+        i++; 
+    };
+  };
